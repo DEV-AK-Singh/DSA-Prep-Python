82cb164f33d78723f2fce7726a282690237d4c82
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-05-30 22:23:23 +0530

    added complexity

---
+++ 14_complexity/main.py
@@ -0,0 +1,9 @@
+# This is a simple Python script to demonstrate a basic loop
+for i in range(5):
+    print(i)
+# O(n) complexity, where n is the number of iterations
+
+for j in range(10):
+    for k in range(5):
+        print(j, k)
+# O(n^2) complexity, where n is the number of iterations
