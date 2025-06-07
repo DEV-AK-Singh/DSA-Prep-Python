3370542bb62f6a5e7e102bcd7fea76b405d48200
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-06-08 03:05:19 +0530

    min max

---
+++ practices/numpy/min_max.py
@@ -0,0 +1,7 @@
+import numpy as np
+n, m = list(map(int, input().split()))
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+my_arr = np.array(arr)
+print(np.max(np.min(my_arr, axis=1)))
