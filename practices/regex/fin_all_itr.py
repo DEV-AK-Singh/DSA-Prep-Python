835fe6079a0ce0e733b811f202bb4f8f303a6644
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-06-09 21:32:36 +0530

    done find all update

--- practices/regex/fin_all_itr.py
+++ practices/regex/fin_all_itr.py
@@ -1,7 +1,7 @@
 import re    
 if __name__ == "__main__":
     s = input()
-    grps = re.findall(r"[^aeiouAEIOU]([aeiouAEIOU]{2,})[^aeiouAEIOU]", s)
+    grps = re.findall(r"(?<=[^aeiouAEIOU])([aeiouAEIOU]{2,})[^aeiouAEIOU]", s)
     if len(grps) > 0:
         print("\n".join(grps))
     else:
