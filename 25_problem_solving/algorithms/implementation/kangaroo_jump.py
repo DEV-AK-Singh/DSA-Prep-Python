0c7191d7bce83afb5f93ce71837ebe76c9814296
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-06-14 02:57:54 +0530

    kangaroo jump problem

---
+++ 25_problem_solving/algorithms/implementation/kangaroo_jump.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    it = 0
+    max_it = 10000
+    while x1 != x2:
+        x1 += v1
+        x2 += v2
+        if it == max_it:
+            return 'NO'
+        it += 1
+    return 'YES' 
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
