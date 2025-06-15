05c139ef3d595675f4eaa218da70830745f186ba
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-06-15 17:32:54 +0530

    done drawing book

---
+++ 25_problem_solving/algorithms/implementation/drawing_book.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p): 
+    turns_total = math.ceil((n-1)/2)
+    turns_start = math.ceil((p-1)/2)
+    turns_end = turns_total - turns_start
+    min_turns = min(turns_start, turns_end)
+    return min_turns
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
