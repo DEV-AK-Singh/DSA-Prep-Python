f0df765538deac98e3f077036a50eaf7f7bf2661
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-06-15 14:56:59 +0530

    socks problem done

---
+++ 25_problem_solving/algorithms/implementation/socks_pair.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    pairs = {}
+    pairs_count = 0
+    for socks in ar:
+        if socks in pairs:
+            pairs[socks] += 1
+        else:
+            pairs[socks] = 1
+    for v in pairs.values():
+        pairs_count += (v//2)
+    return pairs_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
