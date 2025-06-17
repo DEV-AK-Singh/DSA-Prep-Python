ea16986dcbc8bca34dce32982209b2adb24589e2
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-06-18 03:55:57 +0530

    done jumping clouds 2 problem

---
+++ 25_problem_solving/algorithms/implementation/jumping_on_clouds_2.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    jumps = 0
+    start = 0
+    end = len(c)
+    while start != end - 1:
+        if start + 2 < end and c[start+2] != 1:
+            start += 2 
+        else:
+            start += 1
+        jumps += 1 
+    return jumps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
