e7c4ac034bcef690c6d2f8faf0d23f8806580a86
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-06-26 20:55:32 +0530

    done ice cream parlor problem

---
+++ 25_problem_solving/algorithms/searching/icecream_parlor.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr): 
+    for i in range(0, len(arr)): 
+        flavor1 = arr[i]  
+        flavor2 = m - flavor1 
+        for j in range(0, len(arr)):
+            if i != j and arr[j] == flavor2:
+                return [i+1, j+1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
