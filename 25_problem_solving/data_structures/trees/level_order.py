98f9c59b635b1b0049521577daa3dc3ed3f695a8
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-06-30 15:28:12 +0530

    done level order problem

---
+++ 25_problem_solving/data_structures/trees/level_order.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+ 
+def levelOrder(root):
+    arr = []
+    arr.append(root)
+    while len(arr): 
+        for i in range(0, len(arr)):
+            temp = arr[0]
+            print(temp.info, end=' ')
+            arr.remove(arr[0])
+            if temp.left:
+                arr.append(temp.left)
+            if temp.right:
+                arr.append(temp.right)
+         
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+levelOrder(tree.root)
