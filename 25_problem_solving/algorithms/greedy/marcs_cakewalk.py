164c89cffb90ffef2f175113877998a0ccdf2570
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-02 23:06:23 +0530

    done marcs cakewalk problem

---
+++ 25_problem_solving/algorithms/greedy/marcs_cakewalk.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    miles = 0
+    sorted_calorie = sorted(calorie)[::-1]
+    for i in range(len(sorted_calorie)):
+        miles = (miles + sorted_calorie[i] * pow(2, i)) 
+    return miles
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
