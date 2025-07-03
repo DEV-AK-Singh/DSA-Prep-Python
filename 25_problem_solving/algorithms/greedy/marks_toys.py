486cd378f3bbfd655a19e4f5b2ab2b57271e9a4e
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-03 19:34:41 +0530

    done marks and toys problem

---
+++ 25_problem_solving/algorithms/greedy/marks_toys.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumToys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER k
+#
+
+def maximumToys(prices, k):
+    # Write your code here
+    sort_prices = sorted(prices)
+    current_cost = 0
+    total_item = 0
+    for price in sort_prices:
+        cost = current_cost + price
+        if cost <= k:
+            total_item += 1
+            current_cost = cost
+        else:
+            break
+    return total_item        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
