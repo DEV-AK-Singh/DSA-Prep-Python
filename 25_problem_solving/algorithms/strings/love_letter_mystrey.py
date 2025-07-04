4df3b5a5e5e9ac287e9cb9691890a29d90dd606a
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-05 00:05:57 +0530

    done love story mystery problem

---
+++ 25_problem_solving/algorithms/strings/love_letter_mystrey.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'theLoveLetterMystery' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def theLoveLetterMystery(s):
+    s = list(s)
+    i = 0
+    j = len(s)-1
+    steps = 0
+    while i < j: 
+        steps += abs(ord(s[i])-ord(s[j]))
+        i += 1
+        j -= 1 
+    return steps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
